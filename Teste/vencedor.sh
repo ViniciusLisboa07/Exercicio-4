c2d4b34c7834c5d4fec021ba92a6e2c49b374fdb
Vinícius Lisboa <63267152+ViniciusLisboa07@users.noreply.github.com>
2020-09-24 16:11:49 -0300

    Update vencedor.sh

--- Teste/vencedor.sh
+++ Teste/vencedor.sh
@@ -1,4 +1,26 @@
 
 #!/bin/bash 
 
-echo "AAA"
+i=1
+
+for i in $@
+do
+        if [ ${i} -gt $MAIOR ]
+        then
+                MAIOR=${i}
+        fi
+        for j in $@
+        do
+                if [ ${i} -eq ${j} ]
+                then
+                        EMPATE=$(($EMPATE+1))
+                fi
+        done
+        EMPATE=$(($EMPATE-1))
+done
+EMPATE=$(($EMPATE/2))
+
+echo "==-==-==JOGO CONCLUIDO==-==-=="
+echo
+echo "O maior é $MAIOR"
+echo "Empates: $EMPATE"
